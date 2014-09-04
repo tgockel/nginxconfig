f867809f319f8ed596764eb5c15d7aad0c5f38a1
Travis Gockel <travis@gockelhut.com>
2014-09-04 01:37:02 -0600

    Moves the line parsing tests to their own file.

--- src/nginxconfig-tests/main.cpp
+++ src/nginxconfig-tests/main.cpp
@@ -12,39 +12,7 @@
 
 #include <nginxconfig/all.hpp>
 
-#include <nginxconfig/parse_types.hpp>
-
 #include "test.hpp"
-
-using namespace nginxconfig::parser;
-
-TEST(line_components_empty)
-{
-    auto x = line_components::create_from_line("");
-    ensure(line_kind::comment == x.category);
-}
-
-TEST(line_components_comment_only)
-{
-    auto x = line_components::create_from_line("#user html");
-    ensure(line_kind::comment == x.category);
-    ensure_eq(x.comment, "user html");
-}
-
-
-TEST(line_components_comment_only_whitespace_prefix)
-{
-    auto x = line_components::create_from_line("  \t#user html");
-    ensure(line_kind::comment == x.category);
-    ensure_eq(x.comment, "user html");
-}
-
-TEST(line_components_simple_no_comment)
-{
-    auto x = line_components::create_from_line("worker_processes 1;");
-    ensure(line_kind::simple == x.category);
-    ensure_eq(x.name, "worker_processes");
-}
 
 TEST(user_config)
 {
